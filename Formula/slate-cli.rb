aaf9fb2bad0809dc177642b0b440aac9197509b2
Wojciech Protasik <dev@protasik.pl>
2026-05-11 19:57:01 +0200

    slate-cli 0.2.0

--- Formula/slate-cli.rb
+++ Formula/slate-cli.rb
@@ -1,10 +1,10 @@
 class SlateCli < Formula
   desc "Command-line client for Slate (slatetodo.com)"
   homepage "https://github.com/wprodev/slate-todo-cli"
-  url "https://cli.slatetodo.com/download/releases/0.1.0/slate-cli-0.1.0.tar.gz"
-  sha256 "e23a1ba55d78f3acce4a014c3ec163f050bc0b4439eeca8dcb63f1396709a908"
+  url "https://cli.slatetodo.com/download/releases/0.2.0/slate-cli-0.2.0.tar.gz"
+  sha256 "6421bf6c49bb8b2e557cb5a5a91f8fd0f9e9f70d5a403663c3f920ca0145819a"
   license "MIT"
-  version "0.1.0"
+  version "0.2.0"
 
   depends_on "node"
 
